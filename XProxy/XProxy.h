cbc3c730defc9edc5b354dd36c90cd89664d5ad2
networkextension <b@gmail.com>
2017-11-23 10:44:39 +0800

    Initial Commit

---
+++ XProxy/XProxy.h
@@ -0,0 +1,19 @@
+//
+//  XProxy.h
+//  XProxy
+//
+//  Created by yarshure on 2017/11/23.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XProxy.
+FOUNDATION_EXPORT double XProxyVersionNumber;
+
+//! Project version string for XProxy.
+FOUNDATION_EXPORT const unsigned char XProxyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XProxy/PublicHeader.h>
+
+
