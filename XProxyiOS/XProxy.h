89bf24417bc6b270c8f0925441451cd2f00c3d7e
yarshure <support@abigt.net>
2017-12-15 14:45:38 +0800

    update File Name

--- XProxyiOS/XProxy.h
+++ XProxyiOS/XProxy.h
@@ -8,10 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for XProxyiOS.
-FOUNDATION_EXPORT double XProxyiOSVersionNumber;
+//! Project version number for XProxy.
+FOUNDATION_EXPORT double XProxyVersionNumber;
 
-//! Project version string for XProxyiOS.
+//! Project version string for XProxy.
 FOUNDATION_EXPORT const unsigned char XProxyiOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XProxyiOS/PublicHeader.h>
